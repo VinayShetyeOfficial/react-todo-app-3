942363588411953ebfc4db406e63dd99b1f54350
Vinay Shetye <vinay.courses.official@gmail.com>
2025-02-02 03:00:47 -0500

    updated file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'
+/**
+ * Application entry point
+ * Renders the main App component within StrictMode
+ */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
